6260f0b1bf45d623c423349e1835b62adebb52a5
AleksandrKostikov <Kostikov84@gmail.com>
2021-02-18 16:57:55 +0000

    Some changes

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sazerac Laravel - @yield('title')</title>
+    <title>Laravel - @yield('title')</title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
@@ -12,6 +12,14 @@
     <link href="https://fonts.googleapis.com/css?family=Playfair&#43;Display:700,900&amp;display=swap" rel="stylesheet">
     <!-- Custom styles for this template -->
     <link href="/css/blog.css" rel="stylesheet">
+    <style>
+        .completed {
+            text-decoration: line-through;
+        }
+        a {
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
 
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\CompletedStepsController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TasksController;
 use App\Http\Controllers\FeedbacksController;
 use App\Http\Controllers\ArticlesController;
+use App\Http\Controllers\TaskStepsController;
 
 Route::view('/about', 'about');
 Route::view('/contacts', 'contacts');
@@ -17,3 +19,8 @@ Route::get('/articles/{article}', [ArticlesController::class, 'show']);
 
 Route::get('/admin/feedback', [FeedbacksController::class, 'index']);
 Route::post('/contacts', [FeedbacksController::class, 'store']);
+
+Route::post('/tasks/{task}/steps', [TaskStepsController::class, 'store']);
+
+Route::post('/completed-steps/{step}', [CompletedStepsController::class, 'store']);
+Route::delete('/completed-steps/{step}', [CompletedStepsController::class, 'destroy']);
